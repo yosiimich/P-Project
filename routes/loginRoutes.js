62caf3e238540adfab8076bb6c5d6bbcdc3e8186
yosimich <106742142+yosiimich@users.noreply.github.com>
2024-12-05 19:51:43 +0900

    notice
    get notice page

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getMain, getLogin, loginUser, logout, getRegister,registerUser } = require("../controllers/loginController");
+const { getMain, getNotice, getLogin, loginUser, logout, getRegister,registerUser } = require("../controllers/loginController");
 
 router.route("/").get(getMain)
+router.route("/notice").get(getNotice)
 router.route("/login").get(getLogin).post(loginUser);
 router.route("/register").get(getRegister).post(registerUser);
 router.route("/logout").get(logout);
